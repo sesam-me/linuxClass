a2372a0727b11e24071bd86acf540852a42fb58a
sein <lsi6601@gmail.com>
2023-10-06 19:17:24 +0900

    url

--- src/test/java/com/example/linuxclass/LinuxClassApplicationTests.java
+++ src/test/java/com/example/linuxclass/LinuxClassApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.linuxclass;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class LinuxClassApplicationTests {
 
     @Test
     void contextLoads() {
+//        Assertions.assertEquals(1+1,2);
     }
 
 }
